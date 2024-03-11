66bb756c158fefd4980f05c1fc25499bbdd88ce0
noaether <67566122+noaether@users.noreply.github.com>
2024-03-10 21:38:33 -0400

    initial commit

---
+++ src/main/java/ca/noae/advancementhelper/server/AdvancementHelperServerApplication.java
@@ -0,0 +1,13 @@
+package ca.noae.advancementhelper.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancementHelperServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvancementHelperServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ca/noae/advancementhelper/server/AdvancementHelperServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.noae.advancementhelper.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancementHelperServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
